cfa4c4758288c5d45421e5235846bb62b7e0c818
Hemant Kumar Singh <hemantsinghultimate@gmail.com>
2023-08-11 20:41:04 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,177 +1,59 @@
-const express = require('express');
-const mysql = require('mysql');
-const path = require('path');
-const connection = require('./database')
-const session = require('express-session')
+const express = require("express");
+const path = require("path");
 
-
-const directpath = path.join(__dirname,'public');
-
-
-
+const directpath = path.join(__dirname, "public");
 
 const app = express();
 
-
 app.use(express.json());
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use( session({
-    secret: 'secret hemant',
-    resave: true,
-    saveUninitialized : true
-}
-)
-)
-
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 app.use(express.static(directpath));
 
 
+//port 8000 
 
-app.get('/',(req,res)=>{
-    
-     
-    res.render('register');
-
-      
-     
-})
-
-app.get('/register',(req,res)=>{
-    
-     
-    res.render('register');
-
-      
-     
-})
+const PORT = process.env.PORT || 8000;
 
 
 
 
+// home page
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 
-app.post('/register', function(req,res,next){
 
-
-      const username = req.body.name;
-      const email = req.body.email;
-      const pass = req.body.password;
-
-      console.log(req.body);
-   
-       connection.connect(()=>{
-        
-          sql = "Insert Into register SET ? ";
-            
-         connection.query(sql,req.body);
-         
-        res.redirect('/login');
-
-      })
-   
+//-----------------------------------
 
 
 
 
-
-} )
-
+// about page
 
 
-app.get('/login',(req,res)=>{
-    res.render('login');
-})
-
-
-
-app.post('/login',(req,res)=>{
-      
-    let username = req.body.name;
-    let pass = req.body.password;
-   
-     console.log(username);
-     console.log(pass);
-
-    if( username && pass )
-    {
-        
-       
-         connection.query("SELECT * FROM register WHERE name = ? AND password = ?", [username,pass],function(err,result,fields){
-          
-  
-            console.log(err);
-  
-            if(result.length>0) 
-            {
-                req.session.loggedin = true;
-                req.session.name = username;
-                res.redirect('/home');
-            }
-            else {
-                res.send("Incorrect username and password");
-            }
-           
-        
-    
-   
-
-});  
-   
-    } } );
-
-app.get('/home',(req,res)=>{
-    
-  
-    if(req.session.loggedin)
-   {
-    res.render('home',{username: req.session.name})
-   }
-   else 
-   {
-     res.send("wrong username and password");
-   }
-
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
 
 
 
+// ---------------------------------
+
+// project page
 
 
-app.get('/about',(req,res)=>{
-    
-
-    if(req.session.loggedin)
-   {
-    res.render('about',{username: req.session.name})
-   }
-   else 
-   {
-     res.send("wrong username and password");
-   }
-
-     
-})
 
 
-app.get('/projects',(req,res)=>{
-    
-
-    if(req.session.loggedin)
-    {
-     res.render('projects',{username: req.session.name})
-    }
-    else 
-    {
-      res.send("wrong username and password");
-    }
-
-     
-})
+app.get("/projects", (req, res) => {
+  res.render("projects");
+});
 
 
 
@@ -179,24 +61,16 @@ app.get('/projects',(req,res)=>{
 
 
 
+// ---------------------------------
 
-app.get('/contact',(req,res)=>{
-    
-    if(req.session.loggedin)
-   {
-    res.render('contact',{username: req.session.name})
-   }
-   else 
-   {
-     res.send("wrong username and password");
-   }
-})
+// contact page
 
 
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
 
-
-
-
-
-app.listen(8000);
+app.listen(PORT, function(){
+  console.log('listening on 8000');
+});
